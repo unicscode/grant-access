f716e344fb515237ae8179f4c6e5b476b90b035e
unicscode <lounas.koceila@gmail.com>
2023-02-20 14:53:53 +0100

    feat(example): add example

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,31 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { GrantAccess } from '../src';
+import { applyFilter, GrantAccess } from '../src';
 
 const App = () => {
+  const user = {
+    id: 1,
+    roles: ['editor', 'admin'],
+  };
+  return <PirvateComponent user={user} />;
+};
+
+const policy = { where: { id: { _eq: 1 } } };
+
+const PirvateComponent = ({
+  user,
+}: React.PropsWithoutRef<{ user: { id: number; roles: string[] } }>) => {
+  const hasAccess = React.useMemo(() => {
+    return applyFilter(user, policy);
+  }, [user.id]);
+
   return (
     <div>
-      <GrantAccess hasAccess={true}>
+      <GrantAccess
+        hasAccess={hasAccess}
+        otherwiseRender={<h1>Access denied</h1>}
+      >
         <button type="button"> Delete </button>
       </GrantAccess>
     </div>
